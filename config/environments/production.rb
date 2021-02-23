4929bb381cbac058fc4768794237445a921be7bd
JOEL <joel.aguilarg@gmail.com>
2021-02-23 14:32:54 -0600

    add all changes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -116,5 +116,6 @@ Rails.application.configure do
   # these configuration options.
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
-  # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+  # config.active_record.database_resolvconfig.active_job.queue_adapter = :sidekiqer_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+  config.active_job.queue_adapter = :sidekiq
 end
